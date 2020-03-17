70c4986f820d67e0fc0a0a036eebb232b3fb12c1
Ian Culp <icculp@gmail.com>
2020-03-17 23:38:42 +0000

    Align with master

--- test/main-task3.c
+++ test/main-task3.c
@@ -11,7 +11,7 @@ int main(void)
 	char *s = "String";*/
 	int i;
 /* = _printf("I am printing a % %tring full of %characters and %ss, and sometimes a %%.", c, s);*/
-	i = _printf("Number is %b.", 98);
+	i = _printf("Numbers are:\n%b\n%b\n%b\n%b\n%b\n", 98, 123, 1234, 12345,123456);
 	printf("count%d\n", i);
 	return (0);
 }
